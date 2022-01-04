ec8fb20af93a47f623f46adb741dc83562a088af
John Kerl <kerl.john.r@gmail.com>
2022-01-03 20:31:40 -0500

    Add alphabetize/arrange-by-frequency/scramble buttons

--- pangram-checker.js
+++ pangram-checker.js
@@ -1,19 +1,39 @@
 'use strict';
 
+// See index.html for context.
 class PangramChecker {
-  // ----------------------------------------------------------------
-  // PUBLIC METHODS
-
   constructor() {
+    this.ALPHA_ARRAY = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    this.FREQ_ARRAY = ["e","o","t","h","a","s","n","i","r","d","l","u","y","m","w","f","g","c","b","p","k","v","j","q","x","z"];
+    this.SCRAMBLE_ARRAY = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-    this.LOCAL_STORAGE_KEY = document.URL + '-pangram-checker'
-
-    this.ALPHA_VEC = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-    //this.ALPHA_VEC = ["e","o","t","h","a","s","n","i","r","d","l","u","y","m","w","f","g","c","b","p","k","v","j","q","x","z"];
-    this.ALPHA_SET = new Set();
-    for (let c of this.ALPHA_VEC) {
-      this.ALPHA_SET.add(c)
+    this.letters_set = new Set();
+    for (let c of this.ALPHA_ARRAY) {
+      this.letters_set.add(c)
     }
+
+    this.letters_array = [...this.ALPHA_ARRAY]
+  }
+
+  alphabetize() {
+    this.letters_array = [...this.ALPHA_ARRAY]
+  }
+
+  // Credit: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+  shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+
+  arrangeByFrequency() {
+    this.letters_array = [...this.FREQ_ARRAY]
+  }
+
+  scramble() {
+    this.letters_array = [...this.SCRAMBLE_ARRAY]
+    this.shuffleArray(this.letters_array)
   }
 
   getCounts(text) {
@@ -25,7 +45,7 @@ class PangramChecker {
     const lettersPresentVec = [];
     const lettersAbsentVec = [];
 
-    for (let c of this.ALPHA_VEC) {
+    for (let c of this.letters_array) {
       if (lettersPresentSet.has(c)) {
         lettersPresentVec.push(c)
       } else {
